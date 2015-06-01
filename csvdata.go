1271ff03248b98c48509687c41fb7295d2770ff7
hzmnet <johan@ryron.net>
2015-06-01 23:55:34 +0800

    support update
    change to support base  struct
    remove Bom

--- csvdata.go
+++ csvdata.go
@@ -28,9 +28,11 @@ import (
 	"errors"
 	"io"
 	//	"os"
+	"fmt"
 	"reflect"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // The data source is any object that has a Read method which can
@@ -76,26 +78,54 @@ func StrToInt64(s string) int64 {
 	return i
 }
 
-// Creates a new iterator from a Reader source and a user-defined struct.
-func NewReadIter(rdr Reader, ps interface{}) (this *ReadIter, err error) {
+func mapType(aHeader []string, v reflect.Value) (this *ReadIter, err error) {
 	this = new(ReadIter)
 	this.Line = 1
-	this.Headers, err = rdr.Read()
-	this.Reader = rdr
-	if err != nil {
-		this = nil
-		return
-	}
-	st := reflect.TypeOf(ps).Elem()
-	sv := reflect.ValueOf(ps).Elem()
-	nf := st.NumField()
-	this.kinds = make([]int, nf)
-	this.tags = make([]int, nf)
-	this.fields = make([]reflect.Value, nf)
-	for i := 0; i < nf; i++ {
-		f := st.Field(i) //field
+	//	t := v.Type()
 
-		val := sv.Field(i) // field value
+	st := v.Type() // reflect.TypeOf(v).Elem()
+	//sv := h.Elem()
+	//nf := st.NumField()
+	//this.kinds = make([]int, nf)
+	//this.tags = make([]int, nf)
+	//this.fields = make([]reflect.Value, nf)
+
+	for i := 0; i < st.NumField(); i++ {
+		f := st.Field(i)  //field
+		val := v.Field(i) // field value
+
+		// ADD BY HZM
+		if val.Kind() == reflect.Struct {
+			var lTime time.Time
+			//fmt.Println(reflect.TypeOf(lTime), val.Type(), val.Type().ConvertibleTo(reflect.TypeOf(lTime)))
+
+			// 非时间的结构体
+			if !val.Type().ConvertibleTo(reflect.TypeOf(lTime)) {
+				//fmt.Println("time.Time")
+				lParentReadIter, _ := mapType(aHeader, val)
+				//fmt.Println("lParentReadIter", len(lParentReadIter.fields), len(lParentReadIter.kinds))
+				this.fields = append(this.fields, lParentReadIter.fields...)
+				this.kinds = append(this.kinds, lParentReadIter.kinds...)
+				this.tags = append(this.tags, lParentReadIter.tags...)
+				//fmt.Println(len(this.fields), len(this.kinds))
+				continue
+			}
+			/*
+				switch val.Interface().(type) {
+				case time.Time:
+					fmt.Println("time.Time")
+				default:
+					lParentReadIter, _ := mapType(aHeader, val)
+					//fmt.Println("lParentReadIter", len(lParentReadIter.fields), len(lParentReadIter.kinds))
+					this.fields = append(this.fields, lParentReadIter.fields...)
+					this.kinds = append(this.kinds, lParentReadIter.kinds...)
+					this.tags = append(this.tags, lParentReadIter.tags...)
+					fmt.Println(len(this.fields), len(this.kinds))
+					continue
+				}
+			*/
+		}
+
 		// get the corresponding field name and look it up in the headers
 		tag := f.Tag.Get("field")
 		if len(tag) == 0 {
@@ -104,14 +134,18 @@ func NewReadIter(rdr Reader, ps interface{}) (this *ReadIter, err error) {
 				tag = strings.Replace(tag, "_", " ", -1)
 			}
 		}
+
+		// 遍历对比
 		itag := -1
-		for k, h := range this.Headers {
+		for k, h := range aHeader {
+			fmt.Println(tag, h)
 			if strings.EqualFold(h, tag) {
+
 				itag = k
 				break
 			}
 		}
-
+		fmt.Println(f, val, itag)
 		// 判断是否有该Field
 		if itag == -1 {
 			continue
@@ -150,10 +184,119 @@ func NewReadIter(rdr Reader, ps interface{}) (this *ReadIter, err error) {
 				}
 			}
 		}
+		this.fields = append(this.fields, val)
+		this.kinds = append(this.kinds, kind)
+		this.tags = append(this.tags, itag)
+		//	this.kinds[i] = kind
+		//	this.tags[i] = itag
+		//	this.fields[i] = val
+		fmt.Println(val, kind, itag)
+	}
+	fmt.Println(len(this.fields), len(this.kinds))
+	return this, err
+}
+
+// Creates a new iterator from a Reader source and a user-defined struct.
+func NewReadIter(rdr Reader, ps interface{}) (this *ReadIter, err error) {
+	lCsvHeaders, err := rdr.Read()
+
+	// Remove BOM
+	if len(lCsvHeaders) > 0 {
+		lCsvHeaders[0] = strings.Trim(lCsvHeaders[0], "\xef\xbb\xbf")
+	}
+
+	this, _ = mapType(lCsvHeaders, reflect.ValueOf(ps).Elem())
+	this.Reader = rdr
+	if err != nil {
+		this = nil
+		return
+	}
+	//fmt.Println(len(this.fields), len(this.kinds))
+	/*
+		st := reflect.TypeOf(ps).Elem()
+		sv := reflect.ValueOf(ps).Elem()
+		nf := st.NumField()
+		this.kinds = make([]int, nf)
+		this.tags = make([]int, nf)
+		this.fields = make([]reflect.Value, nf)
+		fmt.Println(this.fields, this.tags, this.kinds)
+		for i := 0; i < nf; i++ {
+			f := st.Field(i) //field
+
+			// ADD BY HZM
+			if f.Kind() == reflect.Struct {
+				lParentStruct := f.Type()
+				for _, lFld := range lParentStruct.NumField() {
+					col.FieldName = fmt.Sprintf("%v.%v", t.Field(i).Name, lFld.FieldName)
+					this.fields = append(this.fields)
+				}
+
+				continue
+			}
+
+			fmt.Println(f)
+			val := sv.Field(i) // field value
+			// get the corresponding field name and look it up in the headers
+			tag := f.Tag.Get("field")
+			if len(tag) == 0 {
+				tag = f.Name
+				if strings.Contains(tag, "_") {
+					tag = strings.Replace(tag, "_", " ", -1)
+				}
+			}
+
+			// 遍历对比
+			itag := -1
+			for k, h := range this.Headers {
+				if strings.EqualFold(h, tag) {
+					itag = k
+					break
+				}
+			}
+
+			// 判断是否有该Field
+			if itag == -1 {
+				continue
+				/*
+					err = errors.New("cannot find this field " + tag)
+					this = nil
+					return
+	*/
+	/*			}
+		kind := none_k
+		Kind := f.Type.Kind()
+		// this is necessary because Kind can't tell distinguish between a primitive type
+		// and a type derived from it. We're looking for a Value interface defined on
+		// the pointer to this value
+		_, ok := val.Addr().Interface().(Value)
+		if ok {
+			val = val.Addr()
+			kind = value_k
+		} else {
+			switch Kind {
+			case reflect.Int, reflect.Int16, reflect.Int8, reflect.Int32, reflect.Int64:
+				kind = int_k
+			case reflect.Uint, reflect.Uint16, reflect.Uint8, reflect.Uint32, reflect.Uint64:
+				kind = uint_k
+			case reflect.Float32, reflect.Float64:
+				kind = float_k
+			case reflect.String:
+				kind = string_k
+			default:
+				kind = value_k
+				_, ok := val.Interface().(Value)
+				if !ok {
+					err = errors.New("cannot convert this type ")
+					this = nil
+					return
+				}
+			}
+		}
 		this.kinds[i] = kind
 		this.tags[i] = itag
 		this.fields[i] = val
 	}
+	*/
 	return
 }
 
